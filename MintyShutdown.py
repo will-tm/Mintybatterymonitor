4a92aade97234d5b8a4ff47bd64b3224d05336de
HoolyHoo <ezpz99@gmail.com>
2017-07-23 15:47:13 -0400

    Create MintyShutdown.py

---
+++ MintyShutdown.py
@@ -0,0 +1,12 @@
+from gpiozero import Button
+from subprocess import check_call
+from signal import pause
+
+
+def shutdown():
+    check_call(['sudo', 'poweroff'])
+
+
+shutdown_btn = Button(3, hold_time=1)
+shutdown_btn.when_held = shutdown
+pause()
