d28f532e089cde263f643af7398dcd0841cba974
HoolyHoo <ezpz99@gmail.com>
2017-08-06 11:59:10 -0400

    Update to home directory

--- MintyInstall.sh
+++ MintyInstall.sh
@@ -8,6 +8,7 @@ sudo apt-get install python-pkg-resources python3-pkg-resources -y
 sudo apt-get install -y i2c-tools -y
 sudo apt-get install build-essential python-dev python-smbus python-pip -y
 sudo pip install adafruit-ads1x15
+cd ~
 sudo git clone https://github.com/HoolyHoo/Mintybatterymonitor.git
 sudo chmod 755 /home/pi/Mintybatterymonitor/Pngview/pngview
 sudo chmod 755 /home/pi/Mintybatterymonitor/MintyStart.sh
