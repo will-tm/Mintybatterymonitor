98d1de38c1d7e24a2368aa1934a8be97b8448462
HoolyHoo <ezpz99@gmail.com>
2017-08-10 08:20:09 -0400

    Allow delay for screensaver

--- MintyStart.sh
+++ MintyStart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-python /home/pi/Mintybatterymonitor/MintyBatteryMonitor.py &
 python /home/pi/Mintybatterymonitor/MintyShutdown.py &
+sleep 27
+python /home/pi/Mintybatterymonitor/MintyBatteryMonitor.py &
