621bccd707bb39f0320fcae54bac2d27104d67f2
HoolyHoo <ezpz99@gmail.com>
2017-08-10 13:21:57 -0400

    Added toggle with select button

--- MintyShutdown.py
+++ MintyShutdown.py
@@ -1,12 +1,32 @@
 from gpiozero import Button
 from subprocess import check_call
 from signal import pause
+import os
+import time
+
+
+state = 1
 
 
 def shutdown():
     check_call(['sudo', 'poweroff'])
 
 
+def togglestate():
+    global state
+    if state == 1:
+        os.system('sudo pkill -f "python /home/pi/Mintybatterymonitor/MintyBatteryMonitor.py"')
+        state = 0
+        time.sleep(5)
+
+    else:
+        os.system("python /home/pi/Mintybatterymonitor/MintyBatteryMonitor.py &")
+        state = 1
+        time.sleep(5)
+
+
 shutdown_btn = Button(7, hold_time=1)
+monitor_btn = Button(19, hold_time=2)
 shutdown_btn.when_held = shutdown
+monitor_btn.when_held = togglestate
 pause()
