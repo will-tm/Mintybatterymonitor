c9d2521ead12c5e0b6ebfff4d860422070ae509d
HoolyHoo <ezpz99@gmail.com>
2017-08-21 19:28:44 -0400

    Added toggle that stays active

--- MintyBatteryMonitor.py
+++ MintyBatteryMonitor.py
@@ -10,6 +10,7 @@ import Adafruit_ADS1x15
 warning = 0
 status = 0
 debug = 0
+toggleFile = "/home/pi/Mintybatterymonitor/Toggle.txt"
 PNGVIEWPATH = "/home/pi/Mintybatterymonitor/Pngview/"
 ICONPATH = "/home/pi/Mintybatterymonitor/icons"
 CLIPS = 1
@@ -60,54 +61,107 @@ signal.signal(signal.SIGINT, endProcess)
 # Begin Battery Monitoring
 
 os.system(PNGVIEWPATH + "/pngview -b 0 -l 299999 -x 650 -y 10 " + ICONPATH + "/blank.png &")
+try:
+    with open(toggleFile, 'r') as f:
+        output = f.read()
+except IOError:
+    with open(toggleFile, 'w') as f:
+        f.write('1')
+    output = '1'
+state = int(output)
 
-while True:
-
-    try:
-        ret1 = readVoltage()
-        time.sleep(.4)
-        ret2 = readVoltage()
-        time.sleep(.4)
-        ret3 = readVoltage()
-        time.sleep(.4)
-        ret4 = (ret1 + ret2 + ret3) / 3
-        ret = convertVoltage(ret4)
-        if debug == 1:
-            print(ret)
-        if ret < VOLT0:
-            if status != 0:
-                changeicon("0")
-                if CLIPS == 1:
-                    os.system("/usr/bin/omxplayer --no-osd --layer 999999  " + ICONPATH + "/lowbattshutdown.mp4 --alpha 160;")
-                    voltcheck = readVoltage()
-                    convertFinal = convertVoltage(voltcheck)
-                    if convertFinal <= VOLT50:
-                        os.system("sudo shutdown -h now")
-                    else:
-                        warning = 0
-            status = 0
-        elif ret < VOLT25:
-            if status != 25:
-                changeicon("25")
-                if warning != 1:
+if state == 1:
+    while True:
+        try:
+            ret1 = readVoltage()
+            time.sleep(.4)
+            ret2 = readVoltage()
+            time.sleep(.4)
+            ret3 = readVoltage()
+            time.sleep(.4)
+            ret4 = (ret1 + ret2 + ret3) / 3
+            ret = convertVoltage(ret4)
+            if debug == 1:
+                print(ret)
+            if ret < VOLT0:
+                if status != 0:
+                    changeicon("0")
                     if CLIPS == 1:
-                        os.system("/usr/bin/omxplayer --no-osd --layer 999999  " + ICONPATH + "/lowbattalert.mp4 --alpha 160")
-                    warning = 1
-            status = 25
-        elif ret < VOLT50:
-            if status != 50:
-                changeicon("50")
-            status = 50
-        elif ret < VOLT75:
-            if status != 75:
-                changeicon("75")
-            status = 75
-        else:
-            if status != 100:
-                changeicon("100")
-            status = 100
+                        os.system("/usr/bin/omxplayer --no-osd --layer 999999  " + ICONPATH + "/lowbattshutdown.mp4 --alpha 160;")
+                        voltcheck = readVoltage()
+                        convertFinal = convertVoltage(voltcheck)
+                        if convertFinal <= VOLT75:
+                            os.system("sudo shutdown -h now")
+                        else:
+                            warning = 0
+                status = 0
+            elif ret < VOLT25:
+                if status != 25:
+                    changeicon("25")
+                    if warning != 1:
+                        if CLIPS == 1:
+                            os.system("/usr/bin/omxplayer --no-osd --layer 999999  " + ICONPATH + "/lowbattalert.mp4 --alpha 160")
+                        warning = 1
+                status = 25
+            elif ret < VOLT50:
+                if status != 50:
+                    changeicon("50")
+                status = 50
+            elif ret < VOLT75:
+                if status != 75:
+                    changeicon("75")
+                status = 75
+            else:
+                if status != 100:
+                    changeicon("100")
+                status = 100
 
-        time.sleep(REFRESH_RATE)
-    except IOError:
-        print('No i2c Chip Found!')
-        exit(0)
+            time.sleep(REFRESH_RATE)
+        except IOError:
+            print('No i2c Chip Found!')
+            exit(0)
+
+elif state == 0:
+    while True:
+        try:
+            ret1 = readVoltage()
+            time.sleep(.4)
+            ret2 = readVoltage()
+            time.sleep(.4)
+            ret3 = readVoltage()
+            time.sleep(.4)
+            ret4 = (ret1 + ret2 + ret3) / 3
+            ret = convertVoltage(ret4)
+            if debug == 1:
+                print(ret)
+            if ret < VOLT0:
+                if status != 0:
+                    changeicon("0")
+                    if CLIPS == 1:
+                        os.system("/usr/bin/omxplayer --no-osd --layer 999999  " + ICONPATH + "/lowbattshutdown.mp4 --alpha 160;")
+                        voltcheck = readVoltage()
+                        convertFinal = convertVoltage(voltcheck)
+                        if convertFinal <= VOLT75:
+                            os.system("sudo shutdown -h now")
+                        else:
+                            warning = 0
+                status = 0
+            elif ret < VOLT25:
+                if status != 25:
+                    changeicon("25")
+                    if warning != 1:
+                        if CLIPS == 1:
+                            os.system("/usr/bin/omxplayer --no-osd --layer 999999  " + ICONPATH + "/lowbattalert.mp4 --alpha 160")
+                        warning = 1
+                status = 25
+            elif ret < VOLT50:
+                status = 50
+            elif ret < VOLT75:
+                status = 75
+            else:
+                status = 100
+
+            time.sleep(REFRESH_RATE)
+        except IOError:
+            print('No i2c Chip Found!')
+            exit(0)
