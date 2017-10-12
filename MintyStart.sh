3787da1061e3dbe2392d9344fee730a3a9daaccb
HoolyHoo <ezpz99@gmail.com>
2017-10-12 08:16:27 -0400

    New file reference

--- MintyStart.sh
+++ MintyStart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python /home/pi/Mintybatterymonitor/MintyShutdown.py &
-sleep 27
+python /home/pi/Mintybatterymonitor/MintyCombo.py &
+sleep 20
 python /home/pi/Mintybatterymonitor/MintyBatteryMonitor.py &
